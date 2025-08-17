b0e8fa25534325da35dafcfc49e8d07c241f5103
Ricardo Rausell <momet6@gmail.com>
2025-08-17 19:36:36 +0200

    cards finish

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
-import '../screens.dart';
 
 class HomeScreen extends StatelessWidget {
 
