26890ac4f4a2c4b96f2572d5c1f8f41a7a49b3db
Ricardo Rausell <momet6@gmail.com>
2025-08-19 07:53:07 +0200

    progress indicators terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -31,4 +31,11 @@ const appMenuItems = <MenuItem>[
       link: '/cards',
       icon: Icons.credit_card
   ),
+
+  MenuItem(
+      title: 'Progress Indicators',
+      subTitle: 'Generales y controlados',
+      link: '/progress',
+      icon: Icons.refresh_rounded
+  ),
 ];
--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -25,5 +25,11 @@ final appRouter = GoRouter(
       name: CardsScreen.name,
       builder: (context, state) => CardsScreen(),
     ),
+
+    GoRoute(
+      path: '/progress',
+      name: ProgressScreen.name,
+      builder: (context, state) => ProgressScreen(),
+    ),
   ],
 );
